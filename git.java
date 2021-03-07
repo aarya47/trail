47081663a2486a22758d69f408475f9a3c165631
aarya47 <34829457+aarya47@users.noreply.github.com>
2021-03-07 23:36:13 +0530

    Add files via upload

---
+++ git.java
@@ -0,0 +1,16 @@
+package ex1;
+import java.util.*;
+import java.text.DecimalFormat;
+public class ex1 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("####0.00");
+		// TODO Auto-generated method stub
+		/*double buyingPrice, sellingPrice;
+		buyingPrice=20.54;
+		sellingPrice=30.50;
+		System.out.println("Buying price is "+buyingPrice);
+		System.out.println("Selling price is "+sellingPrice);
+	}
+}
